96087d0c16d1c40932b489161ec07d2abbab7ffe
LikhithaShettyU <likhithashettyu@gmail.com>
2025-08-03 15:39:38 +0530

    Update files and add package.json and .gitignore

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client"; 
 /**
  * 
  * <div id="parent">
@@ -12,16 +14,17 @@
  */ 
 
 const parent = React.createElement(
-    "div", 
-    { id: "parent"},
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm an h1 tag"), 
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ])
- );
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "h1" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h2" }, "I'm Likhitha"),
+  ])
+);
+
         
 console.log(parent);     
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(parent);
