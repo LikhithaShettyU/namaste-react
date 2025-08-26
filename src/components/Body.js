a503396274b0899d57fdb0b6e2c6173d2e2d8c05
LikhithaShettyU <likhithashettyu@gmail.com>
2025-08-26 11:39:25 +0530

    Update files

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,69 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./shimmer";
+
 
 
 const Body = () => {
 
     //Local State variable - super powerful variable
-    const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurant] = useState([]);
+    const [filteredRestaurant, setFilterdRestaurant] = useState([]);
+
+    const [searchText, setSearchText] = useState("");
+
+    // Whenever state variable update, react triggers a renconcilation cycle(re-renders the component)
+    console.log("Body Rendered");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+  
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0440477&lng=77.62086599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    
+    setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilterdRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  
+  };
 
 
 
-    return (
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                  <input 
+                    type="text" 
+                    className="search-box" 
+                    value={searchText} 
+                    onChange={(e) => {
+                      setSearchText(e.target.value);
+                    }}
+                  />
+                  <button 
+                   onClick={() => {
+                    //Filter the restraunt cards and update the ui
+                    // searchText
+                    console.log(searchText);
+
+                    const filteredRestaurant = listOfRestaurants.filter(
+                      (res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                    );
+
+                    setFilterdRestaurant(filteredRestaurant);
+
+                   }}
+                  >
+                    Search
+                  </button>
+                </div>
                 <button 
                     className="filter-btn" 
                     onClick={() => {
@@ -26,7 +77,8 @@ const Body = () => {
                 </button>
             </div>
             <div className="res-container">
-                {listOfRestaurants.map((restaurant) => (
+                {Array.isArray(listOfRestaurants) &&
+                    filteredRestaurant.map((restaurant) => (
                     <RestaurantCard resData={restaurant} key={restaurant.info.id}/>
                 ))}
   
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+    const [btnNameReact, setBtnNameReact ] = useState("Login");
+    console.log("Haeder Render");
+
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -13,6 +18,17 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button 
+                        className="login" 
+                        onClick={() => { 
+                            btnNameReact === "Login"
+                             ? setBtnNameReact("Logout")
+                             : setBtnNameReact("Login");
+                            
+                        }}
+                    >
+                        {btnNameReact}
+                    </button>
                 </ul>
             </div>
 
